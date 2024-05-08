24e890c6b5944a0cb8e93d571a81704070722055
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2024-05-08 14:04:22 +0400

    sdasd

---
+++ src/app/core/table/table.component.ts
@@ -0,0 +1,51 @@
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import BrowserAnimationsModule
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss'],
+  imports: [MatTableModule, MatPaginatorModule, BrowserAnimationsModule],
+})
+export class TableComponent implements AfterViewInit, OnInit, OnChanges {
+  displayedColumns: string[] = [];
+  @Input('dataSource') ELEMENT_DATA!: any;
+  dataSource = new MatTableDataSource<any>(this.ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  ngOnInit(): void {
+    console.log(this.ELEMENT_DATA);
+  }
+  ngOnChanges(): void {
+    if (!this.ELEMENT_DATA) return;
+    Object.keys(this.ELEMENT_DATA[0]).forEach((key) => {
+      this.displayedColumns.push(key);
+    });
+    console.log(this.displayedColumns, this.ELEMENT_DATA);
+
+    this.dataSource = new MatTableDataSource<any>(this.ELEMENT_DATA);
+  }
+  ngAfterViewInit() {
+    console.log(this.ELEMENT_DATA);
+    this.dataSource.paginator = this.paginator;
+  }
+  ngAfterContentInit() {
+    console.log(this.ELEMENT_DATA);
+    this.dataSource.paginator = this.paginator;
+  }
+}
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
